31f383625d804f5e871af597df25e71024dbf21a
MaheelLakshan <MaheelLakshanra@gmail.com>
2025-06-29 11:46:52 +0530

    order-service

---
+++ order_service/index.js
@@ -0,0 +1,38 @@
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+  cliendId: 'order-service',
+  brokers: ['localhost:9094'],
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'order-service' });
+
+const run = async () => {
+  try {
+    await producer.connect();
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: 'payment-successful',
+      fromBeginning: true,
+    });
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const value = message.value.toString();
+        const { userId, cart } = JSON.parse(value);
+
+        const dummyOrderId = '123456789';
+
+        await producer.send({
+          topic: 'order-successful',
+          message: [{ value: JSON.stringify({ userId, orderId: dummyOrderId }) }],
+        });
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
